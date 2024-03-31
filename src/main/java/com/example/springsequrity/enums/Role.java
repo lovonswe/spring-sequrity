1b2141a617f862b6c88077aab79e330e9bdea102
nahid <nahidswe@gmail.com>
2024-03-31 17:00:48 +0600

    user created, Jwt authentication filter in progress

---
+++ src/main/java/com/example/springsequrity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.example.springsequrity.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/springsequrity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.springsequrity.repository;
+
+import com.example.springsequrity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
