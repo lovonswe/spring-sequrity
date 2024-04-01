c98085c155b3c201018dd121c78c26c7c36b9637
nahid <nahidswe@gmail.com>
2024-04-01 18:58:03 +0600

    jwt-service-in-progress

--- src/main/java/com/example/springsequrity/service/JwtService.java
+++ src/main/java/com/example/springsequrity/service/JwtService.java
@@ -8,13 +8,19 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "21982BBE816F81DEA5948D124F4F5";
     public String extractUserName(String token){
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
